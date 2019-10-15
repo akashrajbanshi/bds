22ca0a608164b1e5c8f6bcd7d18a4e06ed0efb4b
akwra3 <akash.rajbanshi@lfg.com>
2019-10-15 16:50:00 -0400

    Added Appointment Dialog box

---
+++ lib/pages/navigationpage.dart
@@ -0,0 +1,46 @@
+import 'package:bds/common/strings.dart';
+import 'package:bds/pages/calendarpage.dart';
+import 'package:bds/pages/messagepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> screens = [CalendarPage(), MessagePage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.appTitle),
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              title: Text(Strings.appointment)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message), title: Text(Strings.message))
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
